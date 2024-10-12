1fe725ac9106c360969f84ef6fc4f02dc64b8746
Azamatjon-developer <azamatergasev01@gmail.com>
2024-10-12 20:05:22 +0500

    signup

--- src/redux/api/index.tsx
+++ src/redux/api/index.tsx
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react'
-// import { logout } from '../slices/authSlice';
 
 const baseQuery = async (args: any, api: any, extraOptions: any) => {
   const { dispatch } = api
--- src/redux/api/product-api.tsx
+++ src/redux/api/product-api.tsx
@@ -17,3 +17,4 @@ export const productApi = api.injectEndpoints({
 export const {
   useCreateUserMutation,
 } = productApi
+
