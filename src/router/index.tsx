5685707699f5cb1cca6789ac2a4cb880d723cff5
Azamatjon-developer <azamatergasev01@gmail.com>
2024-10-16 22:24:38 +0500

    vercalfixed

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,6 @@
 import { useRoutes } from 'react-router-dom'
 import { SuspenseComponent as Suspense } from '../utils'
 import { lazy, LazyExoticComponent } from 'react'
-import Home from '../pages/dashboard/home/Home'
-import Explore from '../pages/dashboard/explore/Explore'
-import People from '../pages/dashboard/people/People'
-import Saved from '../pages/dashboard/saved/Saved'
-import Reels from '../pages/dashboard/reels/Reels'
-import Chats from '../pages/dashboard/chats/Chats'
-import CreatePosts from '../pages/dashboard/createPost/CreatePosts'
-import LogOut from '../pages/dashboard/logOut/LogOut'
-import Settings from '../pages/dashboard/settings/Settings'
 
 const RegisterRoutes = () => {
   const Login: LazyExoticComponent<any> = lazy(() =>
