701c0cda5a267caedeca09365d27117985a10700
Azamatjon-developer <azamatergasev01@gmail.com>
2024-10-16 22:23:58 +0500

    icons

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,12 @@
 import { NavLink } from 'react-router-dom'
 import snap from '../../assets/images/Snap.svg'
 import { useGetUserQuery } from '../../redux/api/user-slice'
-
+import HomeIcon from "../../assets/images/HomeIcon.svg"
+import ExploreIcon from "../../assets/images/ExploreIcon.svg"
+import PeopleIcon from "../../assets/images/PeopleIcon.svg"
+import SavedIcon from "../../assets/images/SavedIcon.svg"
+// import ReelsIcon from "../../assets/images/ReelsIcon.svg"
+// import ChatsIcon from "../../assets/images/ChatsIcon.svg"
 const Sidebar = () => {
   const userData = JSON.parse(localStorage.getItem('userData') || '{}')
   const username = userData?.username || ''
@@ -15,21 +20,27 @@ const Sidebar = () => {
         </div>
           <h3 className="text-white font-bold text-[24px] pt-[20px]">{data?.fullName}</h3>
           <h3 className="text-slate-400 font-bold text-[16px]">{data?.username}</h3>
-
       </div>
       <div className="flex flex-col gap-5 pl-[24px]">
-        <NavLink className={'text-white'} to={'/'}>
+        
+        <NavLink className={'text-white flex items-center gap-2'} to={'/'}>
+        <img src={HomeIcon} alt="home" />
           Home
         </NavLink>
-        <NavLink className={'text-white'} to={'/explore'}>
+
+        <NavLink className={'text-white flex items-center gap-2'} to={'/explore'}>
+        <img src={ExploreIcon} alt="home" />
           Explore
         </NavLink>
-        <NavLink className={'text-white'} to={'/people'}>
-          People
+        <NavLink className={'text-white flex items-center gap-2'} to={'/people'}>
+        <img src={PeopleIcon} alt="home" />
+          People 
         </NavLink>
-        <NavLink className={'text-white'} to={'/saved'}>
-          Saved
+        <NavLink className={'text-white flex items-center gap-2'} to={'/saved'}>
+        <img src={SavedIcon} alt="home" />
+          Saved 
         </NavLink>
+        
         <NavLink className={'text-white'} to={'/reels'}>
           Reels
         </NavLink>
