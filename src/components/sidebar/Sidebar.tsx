18eb060e1598adaa405180cbf560122af756b3ee
Azamatjon-developer <azamatergasev01@gmail.com>
2024-10-18 11:06:20 +0500

    activeDone

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,17 +1,17 @@
-import { NavLink } from 'react-router-dom'
-import snap from '../../assets/images/Snap.svg'
-import { useGetUserQuery } from '../../redux/api/user-slice'
-import HomeIcon from "../../assets/images/HomeIcon.svg"
-import ExploreIcon from "../../assets/images/ExploreIcon.svg"
-import PeopleIcon from "../../assets/images/PeopleIcon.svg"
-import SavedIcon from "../../assets/images/SavedIcon.svg"
-import ReelsIcon from "../../assets/images/ReelsIcon.svg"
-import ChatsIcon from "../../assets/images/ChatsIcon.svg"
-import createIcon from "../../assets/images/CreatePostIcon.svg"
-import logOut from "../../assets/images/LogOutIcon.svg"
-import settings from "../../assets/images/SettingsIcon.svg"
-import { useState } from 'react'
-import { Modal } from 'antd'
+import { NavLink } from 'react-router-dom';
+import snap from '../../assets/images/Snap.svg';
+import { useGetUserQuery } from '../../redux/api/user-slice';
+import HomeIcon from "../../assets/images/HomeIcon.svg";
+import ExploreIcon from "../../assets/images/ExploreIcon.svg";
+import PeopleIcon from "../../assets/images/PeopleIcon.svg";
+import SavedIcon from "../../assets/images/SavedIcon.svg";
+import ReelsIcon from "../../assets/images/ReelsIcon.svg";
+import ChatsIcon from "../../assets/images/ChatsIcon.svg";
+import createIcon from "../../assets/images/CreatePostIcon.svg";
+import logOut from "../../assets/images/LogOutIcon.svg";
+import settings from "../../assets/images/SettingsIcon.svg";
+import { useState } from 'react';
+import { Modal } from 'antd';
 
 const Sidebar = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -29,7 +29,7 @@ const Sidebar = () => {
   };
 
   const handleCancel = () => {
-    setIsModalOpen(false); 
+    setIsModalOpen(false);
   };
 
   const userData = JSON.parse(localStorage.getItem('userData') || '{}');
@@ -37,59 +37,110 @@ const Sidebar = () => {
   const { data } = useGetUserQuery(username);
 
   return (
-    <div className="col-span-2 bg-black overflow-y-auto h-screen ">
-      <div className="pt-[48px] pl-[24px] pr-[24px] pb-[44px] ">
-        <div className="flex items-center gap-[5px]">
-          <img src={snap} alt="snap" />
-          <h2 className="text-white text-[28px] font-bold">Sidebar</h2>
+    <div className="col-span-2 bg-[#1c1c1c] rounded-xl shadow-lg overflow-y-auto h-screen">
+      <div className="pt-12 px-6 pb-10">
+        <div className="flex items-center gap-2 mb-6">
+          <img src={snap} alt="snap" className="w-8 h-8"/>
+          <h2 className="text-white text-2xl font-bold">Sidebar</h2>
         </div>
-        <h3 className="text-white font-bold text-[24px] pt-[20px]">{data?.fullName}</h3>
-        <h3 className="text-slate-400 font-bold text-[16px]">{data?.username}</h3>
+        <div className="text-center">
+          <h3 className="text-white font-semibold text-lg">{data?.fullName}</h3>
+          <p className="text-slate-400 font-medium text-sm">{data?.username}</p>
+        </div>
       </div>
-      <div className="flex flex-col gap-5 pl-[24px]">
-        <NavLink className={'text-white flex items-center gap-2'} to={'/'}>
-          <img src={HomeIcon} alt="home" />
+      <div className="flex flex-col gap-4 px-6">
+        <NavLink
+          className={({ isActive }) =>
+            `text-white flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-[#4b48e5] transition duration-300 ${isActive ? 'bg-[#877EFF]' : ''}`
+          }
+          to={'/'}
+        >
+          <img src={HomeIcon} alt="home" className="w-6 h-6" />
           Home
         </NavLink>
 
-        <NavLink className={'text-white flex items-center gap-2'} to={'/explore'}>
-          <img src={ExploreIcon} alt="explore" />
+        <NavLink
+          className={({ isActive }) =>
+            `text-white flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-[#4b48e5] transition duration-300 ${isActive ? 'bg-[#877EFF]' : ''}`
+          }
+          to={'/explore'}
+        >
+          <img src={ExploreIcon} alt="explore" className="w-6 h-6" />
           Explore
         </NavLink>
-        <NavLink className={'text-white flex items-center gap-2'} to={'/people'}>
-          <img src={PeopleIcon} alt="people" />
+
+        <NavLink
+          className={({ isActive }) =>
+            `text-white flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-[#4b48e5] transition duration-300 ${isActive ? 'bg-[#877EFF]' : ''}`
+          }
+          to={'/people'}
+        >
+          <img src={PeopleIcon} alt="people" className="w-6 h-6" />
           People
         </NavLink>
-        <NavLink className={'text-white flex items-center gap-2'} to={'/saved'}>
-          <img src={SavedIcon} alt="saved" />
+
+        <NavLink
+          className={({ isActive }) =>
+            `text-white flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-[#4b48e5] transition duration-300 ${isActive ? 'bg-[#877EFF]' : ''}`
+          }
+          to={'/saved'}
+        >
+          <img src={SavedIcon} alt="saved" className="w-6 h-6" />
           Saved
         </NavLink>
-        <NavLink className={'text-white flex items-center gap-2'} to={'/reels'}>
-          <img src={ReelsIcon} alt="reels" />
+
+        <NavLink
+          className={({ isActive }) =>
+            `text-white flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-[#4b48e5] transition duration-300 ${isActive ? 'bg-[#877EFF]' : ''}`
+          }
+          to={'/reels'}
+        >
+          <img src={ReelsIcon} alt="reels" className="w-6 h-6" />
           Reels
         </NavLink>
-        <NavLink className={'text-white flex items-center gap-2'} to={'/chats'}>
-          <img src={ChatsIcon} alt="chats" />
+
+        <NavLink
+          className={({ isActive }) =>
+            `text-white flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-[#4b48e5] transition duration-300 ${isActive ? 'bg-[#877EFF]' : ''}`
+          }
+          to={'/chats'}
+        >
+          <img src={ChatsIcon} alt="chats" className="w-6 h-6" />
           Chats
         </NavLink>
-        <NavLink className={'text-white flex items-center gap-2 mb-[108px]'} to={'/createPosts'}>
-          <img src={createIcon} alt="create post" />
+
+        <NavLink
+          className={({ isActive }) =>
+            `text-white flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-[#4b48e5] mb-16 transition duration-300 ${isActive ? 'bg-[#877EFF]' : ''}`
+          }
+          to={'/createPosts'}
+        >
+          <img src={createIcon} alt="create post" className="w-6 h-6" />
           Create Post
         </NavLink>
 
-        <div onClick={showModal} className={'text-white flex items-center gap-2 cursor-pointer'}>
-          <img src={logOut} alt="logout" />
-          LogOut
+        {/** Logout and Settings */}
+        <div
+          onClick={showModal}
+          className="text-white flex items-center gap-3 px-4 py-3 rounded-lg cursor-pointer hover:bg-[#4b48e5] transition duration-300"
+        >
+          <img src={logOut} alt="logout" className="w-6 h-6" />
+          Log Out
         </div>
 
-        <NavLink className={'text-white flex items-center gap-2'} to={'/settings'}>
-          <img src={settings} alt="settings" />
+        <NavLink
+          className={({ isActive }) =>
+            `text-white flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-[#4b48e5] transition duration-300 ${isActive ? 'bg-[#877EFF] rounded-xl' : ''}`
+          }
+          to={'/settings'}
+        >
+          <img src={settings} alt="settings" className="w-6 h-6" />
           Settings
         </NavLink>
 
         <Modal
           title="Are you sure you want to log out?"
-          open={isModalOpen} 
+          open={isModalOpen}
           onOk={handleOk}
           onCancel={handleCancel}
         >
