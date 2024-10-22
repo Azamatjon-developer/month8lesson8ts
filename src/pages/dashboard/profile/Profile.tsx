dfd88d4a4f83687ced2a9f3907edad8b953b09f3
Azamatjon-developer <azamatergasev01@gmail.com>
2024-10-22 18:38:32 +0500

    videoDone

--- src/pages/dashboard/profile/Profile.tsx
+++ src/pages/dashboard/profile/Profile.tsx
@@ -1,18 +1,21 @@
-import { useGetAllPostsQuery, useGetUserNameQuery } from "../../../redux/api/user-slice";
-import noImage from "../../../assets/images/noImage.jpg";
+import {
+  useGetAllPostsQuery,
+  useGetUserNameQuery,
+} from '../../../redux/api/user-slice'
+import noImage from '../../../assets/images/noImage.jpg'
 
 const Profile = () => {
   const currentUsername = window.localStorage.getItem('userData')
     ? JSON.parse(window.localStorage.getItem('userData') as string).username
-    : null;
-  const { data } = useGetUserNameQuery(currentUsername);
-  const { data: posts } = useGetAllPostsQuery(currentUsername);
-  console.log(posts);
+    : null
+  const { data } = useGetUserNameQuery(currentUsername)
+  const { data: posts } = useGetAllPostsQuery(currentUsername)
+  console.log(posts)
 
   return (
     <div className="h-screen overflow-y-auto bg-black text-white">
-      <div className="max-w-[1050px]  rounded-md shadow-lg pt-[60px] pl-[80px] ">
-          <div className="flex items-center gap-8 mb-[30px]">
+      <div className="w-full p-[40px]  rounded-md shadow-lg pt-[60px] pl-[80px] ">
+        <div className="flex items-center gap-8 mb-[30px]">
           <div className="relative">
             <img
               onError={(e) => (e.currentTarget.src = noImage)}
@@ -23,11 +26,14 @@ const Profile = () => {
           </div>
           <div className="text-left">
             {data?.fullName && (
-              <h3 className="text-[36px] font-bold mb-2 text-white">{data.fullName}</h3>
+              <h3 className="text-[36px] font-bold mb-2 text-white">
+                {data.fullName}
+              </h3>
             )}
             {data?.username && (
               <p className="text-[#7878A3] text-[18px] mb-[20px]">
-                <span className="font-medium">Username: @</span>{data.username}
+                <span className="font-medium">Username: @</span>
+                {data.username}
               </p>
             )}
             {data?.email && (
@@ -37,11 +43,15 @@ const Profile = () => {
             )}
             <div className="flex items-center gap-8 mt-6">
               <div className="text-center">
-                <p className="font-bold text-xl text-white">{data?.followers?.length || 0}</p>
+                <p className="font-bold text-xl text-white">
+                  {data?.followers?.length || 0}
+                </p>
                 <p className="text-sm text-gray-400">Followers</p>
               </div>
               <div className="text-center">
-                <p className="font-bold text-xl text-white">{data?.following?.length || 0}</p>
+                <p className="font-bold text-xl text-white">
+                  {data?.following?.length || 0}
+                </p>
                 <p className="text-sm text-gray-400">Following</p>
               </div>
             </div>
@@ -49,23 +59,32 @@ const Profile = () => {
         </div>
 
         <h2 className="text-white text-[30px] font-semibold mb-8">Posts</h2>
-        <div className="grid grid-cols-4 gap-6 w-[1000px]">
+        <div className="grid grid-cols-3 gap-6 ">
           {posts?.map((post: any, index: number) => (
             <div
               key={index}
-              className=" rounded-lg  shadow-lg transform hover:scale-105 transition-transform duration-300"
+              className=" rounded-lg w-[90%] shadow-lg transform hover:scale-105 transition-transform duration-300"
             >
               <div className="p-4 shadow-lg">
-                <img
-                  src={post?.content[0]?.url || noImage}
-                  alt={post?.content_alt || "Post image"}
-                  className="w-full h-48 object-cover rounded-t-lg"
-                />
+                {post?.content[0]?.type == 'IMAGE' ? (
+                  <img
+                    src={post?.content[0]?.url || noImage}
+                    alt={post?.content_alt || 'Post image'}
+                    className="w-full object-cover rounded-lg"
+                  />
+                ) : (
+                  <div>
+                    <video controls src={post?.content[0]?.url}></video>
+                  </div>
+                )}
+
                 <div className="pt-[12px]">
                   <h3 className="text-xl font-semibold text-white mb-2 truncate">
                     {post?.content_alt || 'Untitled'}
                   </h3>
-                  <p className="text-gray-400 mb-1">Location: {post?.location || 'Unknown'}</p>
+                  <p className="text-gray-400 mb-1">
+                    Location: {post?.location || 'Unknown'}
+                  </p>
                 </div>
               </div>
             </div>
@@ -73,7 +92,7 @@ const Profile = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Profile;
+export default Profile
